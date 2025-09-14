40fb71b72a5a453c58bbd1ba280c744e5229d664
Hana <hanamarwasadek2004@gmail.com>
2025-09-14 21:02:26 +0300

    create ITI database by convention

---
+++ EF#01/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_01.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Hiring_Date { get; set; }
+        public int Ins_Id { get; set; }
+    }
+}
---
+++ EF#01/Models/Stud_Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_01.Models
+{
+    internal class Stud_Course
+    {
+        public int Stud_CourseId { get; set; }
+        public int Course_Id { get; set; }
+        public string Grade { get; set; }
+    }
+}
