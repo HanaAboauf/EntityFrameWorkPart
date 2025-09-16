cdd86213042715fbd8431cfd72f39104f983b5e7
Hana <hanamarwasadek2004@gmail.com>
2025-09-16 15:27:34 +0300

    Apply fluent API mapping using separate class configrations

--- EF#02/DatabaseContexts/CompanyDbContext.cs
+++ EF#02/DatabaseContexts/CompanyDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,26 +22,46 @@ namespace EF_02.DatabaseContexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employess>().ToTable("EmployeeTable");
-            modelBuilder.Entity<Employess>().HasKey(E => E.EmpId);
-            modelBuilder.Entity<Employess>().Property(e => e.Name).HasColumnName("EmployeeName").
-                HasMaxLength(50).IsRequired();
-            modelBuilder.Entity<Employess>().Property(e => e.Position)
-                .HasMaxLength(50);
-            modelBuilder.Entity<Employess>()
-                .Property(e => e.Salary)
-                .HasColumnType("decimal(18,2)"); // also I can use HasPrecision(18,2)
-            modelBuilder.Entity<Employess>()
-                .ToTable(t=>t.HasCheckConstraint("validate Email", "[Email] like '%@%.%'"));
-            modelBuilder.Entity<Employess>()
-                .ToTable(t => t.HasCheckConstraint("validate Salary", "[Salary] > 0"));
-            modelBuilder.Entity<Employess>().ToTable(t => t.HasCheckConstraint("validate Age", "[Age] >= 18 and [Age]<=30"));
-            modelBuilder.Entity<Employess>().Property(e => e.Department).HasMaxLength(50);
-            modelBuilder.Entity<Employess>().Property(e => e.Phone).HasMaxLength(11);
-            modelBuilder.Entity<Employess>().Property(e => e.HireDate)
-                .HasAnnotation("DataType", "DateTime")
-                .HasDefaultValueSql("getdate()");// this method to set the time when the record is created
-              //.HasComputedColumnSql("getdate()");// this method to set the time when the record is created and updated
+
+            /* for each entity class I create a class that implement IEntityTypeConfiguration<T> interface
+             * but this defect is that I should apply this modelBuilder.ApplyConfiguration for each entity class
+             */
+
+
+            //modelBuilder.ApplyConfiguration<Employess>(new ModelConfigrations.EmployeeConfigrations());
+
+
+
+            // this method to apply all the configurations classes that implement IEntityTypeConfiguration<T> interface
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+
+
+            #region Fluent API Traditional way
+
+            //modelBuilder.Entity<Employess>().ToTable("EmployeeTable");
+            //modelBuilder.Entity<Employess>().HasKey(E => E.EmpId);
+            //modelBuilder.Entity<Employess>().Property(e => e.Name).HasColumnName("EmployeeName").
+            //    HasMaxLength(50).IsRequired();
+            //modelBuilder.Entity<Employess>().Property(e => e.Position)
+            //    .HasMaxLength(50);
+            //modelBuilder.Entity<Employess>()
+            //    .Property(e => e.Salary)
+            //    .HasColumnType("decimal(18,2)"); // also I can use HasPrecision(18,2)
+            //modelBuilder.Entity<Employess>()
+            //    .ToTable(t=>t.HasCheckConstraint("validate Email", "[Email] like '%@%.%'"));
+            //modelBuilder.Entity<Employess>()
+            //    .ToTable(t => t.HasCheckConstraint("validate Salary", "[Salary] > 0"));
+            //modelBuilder.Entity<Employess>().ToTable(t => t.HasCheckConstraint("validate Age", "[Age] >= 18 and [Age]<=30"));
+            //modelBuilder.Entity<Employess>().Property(e => e.Department).HasMaxLength(50);
+            //modelBuilder.Entity<Employess>().Property(e => e.Phone).HasMaxLength(11);
+            //modelBuilder.Entity<Employess>().Property(e => e.HireDate)
+            //    .HasAnnotation("DataType", "DateTime")
+            //    .HasDefaultValueSql("getdate()");// this method to set the time when the record is created
+            //  //.HasComputedColumnSql("getdate()");// this method to set the time when the record is created and updated 
+            #endregion
 
         }
     }
---
+++ EF#02/ModelConfigrations/EmployeeConfigrations.cs
@@ -0,0 +1,38 @@
+﻿using EF_02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_02.ModelConfigrations
+{
+    internal class EmployeeConfigrations : IEntityTypeConfiguration<Employess>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Employess> builder)
+        {
+            builder.ToTable("EmployeeTable");
+            builder.HasKey(E => E.EmpId);
+            builder.Property(e => e.Name).HasColumnName("EmployeeName").
+                HasMaxLength(50).IsRequired();
+            builder.Property(e => e.Position)
+                .HasMaxLength(50);
+            builder
+                .Property(e => e.Salary)
+                .HasColumnType("decimal(18,2)"); // also I can use HasPrecision(18,2)
+            builder
+                 .ToTable(t => t.HasCheckConstraint("validate Email", "[Email] like '%@%.%'"));
+            builder
+                .ToTable(t => t.HasCheckConstraint("validate Salary", "[Salary] > 0"));
+            builder.ToTable(t => t.HasCheckConstraint("validate Age", "[Age] >= 18 and [Age]<=30"));
+            builder.Property(e => e.Department).HasMaxLength(50);
+            builder.Property(e => e.Phone).HasMaxLength(11);
+            builder.Property(e => e.HireDate)
+                .HasAnnotation("DataType", "DateTime")
+                .HasDefaultValueSql("getdate()");// this method to set the time when the record is created
+                                                 //.HasComputedColumnSql("getdate()");// this method to set the time when the record is created and updated
+        }
+    }
+}
