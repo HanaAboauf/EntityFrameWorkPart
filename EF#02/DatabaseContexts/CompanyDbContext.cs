437010e7136d2f7b272d126a1e4ac0c67fa68fb5
Hana <hanamarwasadek2004@gmail.com>
2025-09-16 15:12:50 +0300

    mapping using traditional way of fluent API

---
+++ EF#02/DatabaseContexts/CompanyDbContext.cs
@@ -0,0 +1,47 @@
+﻿using EF_02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_02.DatabaseContexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-TN70H9CL; Database= CompanyDB; Trusted_Connection= true; TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employess>().ToTable("EmployeeTable");
+            modelBuilder.Entity<Employess>().HasKey(E => E.EmpId);
+            modelBuilder.Entity<Employess>().Property(e => e.Name).HasColumnName("EmployeeName").
+                HasMaxLength(50).IsRequired();
+            modelBuilder.Entity<Employess>().Property(e => e.Position)
+                .HasMaxLength(50);
+            modelBuilder.Entity<Employess>()
+                .Property(e => e.Salary)
+                .HasColumnType("decimal(18,2)"); // also I can use HasPrecision(18,2)
+            modelBuilder.Entity<Employess>()
+                .ToTable(t=>t.HasCheckConstraint("validate Email", "[Email] like '%@%.%'"));
+            modelBuilder.Entity<Employess>()
+                .ToTable(t => t.HasCheckConstraint("validate Salary", "[Salary] > 0"));
+            modelBuilder.Entity<Employess>().ToTable(t => t.HasCheckConstraint("validate Age", "[Age] >= 18 and [Age]<=30"));
+            modelBuilder.Entity<Employess>().Property(e => e.Department).HasMaxLength(50);
+            modelBuilder.Entity<Employess>().Property(e => e.Phone).HasMaxLength(11);
+            modelBuilder.Entity<Employess>().Property(e => e.HireDate)
+                .HasAnnotation("DataType", "DateTime")
+                .HasDefaultValueSql("getdate()");// this method to set the time when the record is created
+              //.HasComputedColumnSql("getdate()");// this method to set the time when the record is created and updated
+
+        }
+    }
+}
---
+++ EF#02/Migrations/20250916011337_create companyDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_02.Migrations
+{
+    /// <inheritdoc />
+    public partial class createcompanyDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeTable",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    HireDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()"),
+                    Department = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTable", x => x.EmpId);
+                    table.CheckConstraint("validate Age", "[Age] >= 18 and [Age]<=30");
+                    table.CheckConstraint("validate Email", "[Email] like '%@%.%'");
+                    table.CheckConstraint("validate Salary", "[Salary] > 0");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeTable");
+        }
+    }
+}
