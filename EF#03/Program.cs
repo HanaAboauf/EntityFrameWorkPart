a0d17e7a80a3b5e7818d154031189c5150597a63
Hana <hanamarwasadek2004@gmail.com>
2025-09-20 12:16:04 +0300

    solve question G

--- EF#03/Program.cs
+++ EF#03/Program.cs
@@ -99,6 +99,21 @@ namespace EF_03
             //    Console.WriteLine($"Airline: {item.AirlineName}, Employee Count: {item.EmployeeCount}");
             //}
             #endregion
+
+            #region Question G
+
+            var aircraft = dbcontext.AirCrafts.Where(ac => ac.Model == "Model01");
+
+            if (aircraft != null)
+            {
+                foreach (var ac in aircraft)
+                {
+                    ac.Capacity = 200;
+                }
+                dbcontext.SaveChanges();
+            }
+
+            #endregion
         }
     }
 }
